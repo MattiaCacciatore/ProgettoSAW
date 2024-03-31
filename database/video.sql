7b743846274b9290232ca1a678ede0cefd0a59d5
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-31 09:47:48 +0200

    Create video.sql

---
+++ database/video.sql
@@ -0,0 +1,7 @@
+CREATE TABLE video (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title_course VARCHAR(255) NOT NULL,
+  duration_course INT NOT NULL,
+  type_video VARCHAR(50) NOT NULL,
+  file_video VARCHAR(255) NOT NULL
+);
