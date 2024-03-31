59dce6e744f91ec8c7c217c653e169cb1efee044
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-31 09:58:30 +0200

    Update course_video.rel.sql

--- database/course_video.rel.sql
+++ database/course_video.rel.sql
@@ -4,6 +4,6 @@ CREATE TABLE course_video (
 
   PRIMARY KEY (id_corso, id_video), -- ho visto che 
 
-  FOREIGN KEY (id_corso) REFERENCES courses(id),
+  FOREIGN KEY (id_corso) REFERENCES course(id),
   FOREIGN KEY (id_video) REFERENCES video(id)
 );
