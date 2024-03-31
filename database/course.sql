175c85b73f67e80f36ee4dcd558fa13f1f9b3113
Mattia Cacciatore <cacciatore1995@hotmail.it>
2024-03-31 10:22:10 +0200

    Update course

--- database/course.sql
+++ database/course.sql
@@ -4,7 +4,10 @@ CREATE TABLE course(
     description_of_course VARCHAR(1500) NOT NULL,
     --numer_of_videos is duration
     number_of_videos INT NOT NULL,
-    price DECIMAL(4,2) NOT NULL,
-    average_evaluation DECIMAL(2,1),
-    PRIMARY KEY (id)
+    price DECIMAL(4,2) UNSIGNED NOT NULL,
+    average_evaluation DECIMAL(2,1) UNSIGNED,
+
+    PRIMARY KEY (id),
+
+    CONSTRAINT evaluation_in_range CHECK (average_evaluation < 10.0)
 );
