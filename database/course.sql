913d69edcb9fa3df3265c70c6d54b142e644abd3
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-31 10:44:31 +0200

    Update course.sql

--- database/course.sql
+++ database/course.sql
@@ -3,9 +3,9 @@ CREATE TABLE course (
   name_course VARCHAR(100) NOT NULL,
   description_of_course VARCHAR(1500) NOT NULL,
 
-  number_of_video UNSIGNED INT NOT NULL, -- durata 
+  number_of_video INT UNSIGNED NOT NULL, -- durata 
   price DECIMAL(4,2) UNSIGNED NOT NULL,
-  average_evaluation TINYINT UNSIGNED NOT NULL CHECK (vote <= 5),
+  average_evaluation TINYINT UNSIGNED NOT NULL CHECK (average_evaluation <= 5),
 
   PRIMARY KEY (id)
 );
