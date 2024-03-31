6c57173933fbbe9b36ad01f897d3c1d7f22db294
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-31 10:53:31 +0200

    Update course_video.rel.sql

--- database/course_video.rel.sql
+++ database/course_video.rel.sql
@@ -2,7 +2,7 @@ CREATE TABLE course_video (
   id_corso BIGINT,
   id_video BIGINT,
 
-  PRIMARY KEY (id_corso, id_video), -- ho visto che 
+  PRIMARY KEY (id_corso, id_video),
 
   FOREIGN KEY (id_corso) REFERENCES course(id) ON DELETE SET NULL,
   FOREIGN KEY (id_video) REFERENCES video(id) ON DELETE SET NULL
