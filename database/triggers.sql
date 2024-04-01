3b8f69d7633392bd0b80100c090b2d087ad5707f
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-04-01 10:17:09 +0200

    Update triggers.sql

--- database/triggers.sql
+++ database/triggers.sql
@@ -6,7 +6,7 @@ BEGIN
 	IF NEW.user IN (SELECT follow.user FROM follow WHERE NEW.course = follow.course) THEN
     	INSERT INTO evaluate (user, course, vote, feedback) VALUES(NEW.user, NEW.course, NEW.vote, NEW.feedback);
 	ELSE
-    	SIGNAL SQLSTATE '09000' SET MESSAGE_TEXT = 'This user is ineligible to vote on this course because they did not follow it.';
+    	SIGNAL SQLSTATE '09000' SET MESSAGE_TEXT = 'This user cannot evaluate this course because has not followed it';
 	END IF;
 END;
 //
