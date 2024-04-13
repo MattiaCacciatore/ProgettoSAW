c73d81ed3ba91f67f1c5af48314441671a5401b6
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-04-13 12:44:29 +0200

    Create search_script.php

---
+++ internal_search_system/php/search_script.php
@@ -0,0 +1,13 @@
+<?php
+
+
+include_once '../../configuration/databaseHandler.php';
+
+
+/* non definisco un metodo, perhce' questo file racchiude lo script che utilizza
+ajax per effettuare la richiesta al server, e nient'altro */
+
+
+
+
+?>
