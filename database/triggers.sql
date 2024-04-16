ed3e5690847f2cfbabd459c51f3abd348bd3e689
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2024-04-16 16:41:45 +0200

    Update triggers.sql

--- database/triggers.sql
+++ database/triggers.sql
@@ -1,14 +1,33 @@
+/* Vincolo V1. */
 DELIMITER //
-CREATE TRIGGER check_follower
+CREATE OR REPLACE TRIGGER update_average_evaluation
+AFTER INSERT ON evaluate
+FOR EACH ROW
+BEGIN
+	DECLARE new_average_eval DECIMAL(2,1) UNSIGNED;
+
+	SELECT AVG(evaluate.vote) INTO new_average_eval FROM evaluate WHERE evaluate.id_course = NEW.id_course;
+
+    	UPDATE course SET course.average_evaluation = new_average_eval WHERE course.id = NEW.id_course;
+END;
+//
+/* Vincolo V3. Il codice 09000 indica una eccezione sollevata dall'azione di un trigger. */
+DELIMITER //
+CREATE OR REPLACE TRIGGER check_follower
 BEFORE INSERT ON evaluate
 FOR EACH ROW
 BEGIN
-	IF NEW.user IN (SELECT follow.user FROM follow WHERE NEW.course = follow.course) THEN
-    	INSERT INTO evaluate (user, course, vote, feedback) VALUES(NEW.user, NEW.course, NEW.vote, NEW.feedback);
-	ELSE
-    	SIGNAL SQLSTATE '09000' SET MESSAGE_TEXT = 'This user cannot evaluate this course because has not followed it';
+	IF NEW.email_user NOT IN (SELECT follow.email_user FROM follow WHERE NEW.id_course = follow.id_course) THEN
+    		SIGNAL SQLSTATE '09000' SET MESSAGE_TEXT = 'This user cannot evaluate this course because has not followed it';
 	END IF;
 END;
 //
--- Messo in pratica il vincolo V3 (vedere specifica su docs.google.com)
--- Il codice 09000 indica una eccezione sollevata dall'azione di un trigger.
+/* Vincolo durata in ore dei video di un corso. */
+DELIMITER //
+CREATE OR REPLACE TRIGGER update_duration
+BEFORE INSERT ON video
+FOR EACH ROW
+BEGIN
+    	UPDATE course SET course.duration = course.duration + (NEW.duration / 60) WHERE course.id = NEW.id_course;
+END;
+//
