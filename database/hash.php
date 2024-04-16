c7c225db9e5ba1862b6efd746e876d70904482fe
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2024-04-16 16:42:02 +0200

    Update hash.php

--- database/hash.php
+++ database/hash.php
@@ -1,5 +1,4 @@
 <?php
-	# $password = escapeshellcmd('C:\Users\cacci\Desktop\prova.py'); 
 	$password = $argv[1];
 	$hashed_password = password_hash($password, PASSWORD_DEFAULT); 
 	echo $hashed_password; 
