8be0fc637ba4a24b78d7be245018daab4064d385
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2024-04-16 16:38:12 +0200

    Update belong.rel.sql

--- database/belong.rel.sql
+++ database/belong.rel.sql
@@ -1,9 +1,9 @@
-CREATE TABLE belong (
-  id_corso BIGINT UNSIGNED,
+CREATE TABLE belong(
+  id_course BIGINT UNSIGNED,
   field_name VARCHAR(100),
 
-  PRIMARY KEY (id_corso, field_name),  
+  PRIMARY KEY (id_course, field_name),  
 
-  FOREIGN KEY (id_corso) REFERENCES course(id) ON DELETE CASCADE,
+  FOREIGN KEY (id_course) REFERENCES course(id) ON DELETE CASCADE,
   FOREIGN KEY (field_name) REFERENCES field(field_name) ON DELETE CASCADE
 );
