61a46890dc1e82fc4cdd851140b3c26dba01e536
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2024-04-16 16:39:05 +0200

    Update evaluate.rel.sql

--- database/evaluate.rel.sql
+++ database/evaluate.rel.sql
@@ -1,13 +1,11 @@
-CREATE TABLE evaluate (
-    user VARCHAR(100),
-    course BIGINT UNSIGNED,
+CREATE TABLE evaluate(
+    email_user VARCHAR(100),
+    id_course BIGINT UNSIGNED,
+    feedback VARCHAR(1500),
+    vote TINYINT UNSIGNED NOT NULL CHECK (vote <= 5),
 
-    PRIMARY KEY (user, course),
+    PRIMARY KEY (email_user, id_course),
 
-    FOREIGN KEY (user) REFERENCES user (email) ON DELETE CASCADE,
-    FOREIGN KEY (course) REFERENCES course (id) ON DELETE CASCADE,
-
-    vote TINYINT UNSIGNED NOT NULL CHECK (vote <= 5), 
-    feedback VARCHAR(1500)
-
+    FOREIGN KEY (email_user) REFERENCES user (email) ON DELETE CASCADE,
+    FOREIGN KEY (id_course) REFERENCES course (id) ON DELETE CASCADE
 );
