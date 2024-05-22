08357d78d62fef65a215c9d794d04e78dac8fcb2
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-05-22 18:05:04 +0200

    updates

--- modules/footer.php
+++ modules/footer.php
@@ -4,39 +4,37 @@
 	print(
 	'
 	<footer>
-	<div class="container">
-		<div class="footer-content">
-			<div class="footer-section about">
-				<h3>About STEMazing Education</h3>
-				<p>STEMazing Education offre corsi di alta qualità in Scienza, Tecnologia, Ingegneria e Matematica per studenti e professionisti di tutto il mondo.</p>
-				<div class="socials">
-					<a href="#"><i class="fab fa-facebook-f"></i></a>
-					<a href="#"><i class="fab fa-twitter"></i></a>
-					<a href="#"><i class="fab fa-linkedin-in"></i></a>
-					<a href="#"><i class="fab fa-instagram"></i></a>
-				</div>
-			</div>
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-section about">
+                <h3>About STEMazing Education</h3>
+                <p>STEMazing Education offre corsi di alta qualità in Scienza, Tecnologia, Ingegneria e Matematica per studenti e professionisti di tutto il mondo.</p>
+                <div class="socials">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
 
+            <div class="footer-section contact">
+                <h3>Contact Us</h3>
+                <p>
+                    <i class="fas fa-map-marker-alt"></i> Genova
+                </p>
+                <p>
+                    <i class="fas fa-phone"></i> +39 3234 567 8901
+                </p>
+                <p>
+                    <i class="fas fa-envelope"></i> info@stemazingeducation.com
+                </p>
+            </div>
+        </div>
 
-
-			<div class="footer-section contact">
-				<h3>Contact Us</h3>
-				<p>
-					<i class="fas fa-map-marker-alt"></i> Genova
-				</p>
-				<p>
-					<i class="fas fa-phone"></i> +39 3234 567 8901
-				</p>
-				<p>
-					<i class="fas fa-envelope"></i> info@stemazingeducation.com
-				</p>
-			</div>
-		</div>
-
-		<div class="footer-bottom">
-			<p>&copy; 2024 STEMazing Education. All rights reserved.</p>
-		</div>
-	</div>
+        <div class="footer-bottom">
+            <p>&copy; 2024 STEMazing Education. All rights reserved.</p>
+        </div>
+    </div>
 </footer>
 	'
 	);
